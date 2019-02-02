1c836d929d29f598a9656801b92c94000d758e0f
SkyOne <skyone@gmail.com>
2019-02-02 13:22:09 +0300

    Initial structure

---
+++ src/main/java/sky/one/swkpc/SwkPcApplication.java
@@ -0,0 +1,14 @@
+package sky.one.swkpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwkPcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwkPcApplication.class, args);
+    }
+
+}
+
